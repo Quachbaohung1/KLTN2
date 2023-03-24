f8e5358f6b899ee985e3df4f9393a49c9572907a
Quachbaohung1 <Baohung9715@gmail.com>
2023-03-24 18:02:20 +0700

    second commit

--- login/main.py
+++ login/main.py
@@ -141,43 +141,16 @@ def load_users():
         return render_template('user.html', employee=employee)
     return redirect(url_for('login'))
 
-@app.route('/login/add', methods=["GET", "POST"])
-def add():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        # Hash password using SHA256
-        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
-        is_active = 1
-        # Get the maximum employee_id in the database
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        max_employee_id = get_max_employee_id(cursor)
-        # Generate a new employee_id
-        employee_id = max_employee_id + 1
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesn't exist and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES ( NULL, %s, %s, %s, %s)', (username, password, employee_id, is_active,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-    elif request.method == "POST":
-        # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
-    return render_template('register.html', msg=msg)
+# Xử lý yêu cầu xoá tài khoản
+@app.route('/delete_account', methods=['POST'])
+def delete_account():
+    id = request.form['id']
+    cursor = mysql.cursor()
+    sql = "DELETE FROM `employee` WHERE `id` = %s"
+    val = (id,)
+    cursor.execute(sql, val)
+    mysql.commit()
+    return "Employee has been deleted successfully!"
 
 @app.route('/', methods=['POST'])
 def show_popup():
