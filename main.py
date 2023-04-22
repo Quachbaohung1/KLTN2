d908533e8edd1bccf9634824a128b17c6e19b96a
Quachbaohung1 <Baohung9715@gmail.com>
2023-04-22 18:42:30 +0700

    3st commit

--- main.py
+++ main.py
@@ -91,6 +91,94 @@ def home():
         return render_template('home.html', account=account)
     return redirect(url_for('login'))
 
+@app.route('/login/profile')
+def profile():
+    # Check if user is logged-in
+    if 'loggedin' in session:
+        # We need all the account info for the user, so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Employee WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not logged-in redirect to login page
+    return redirect(url_for('login'))
+
+# Get the maximum employee_id in the database
+def get_max_employee_id(cursor):
+    cursor.execute('SELECT MAX(employee_id) FROM Auth_user')
+    result = cursor.fetchone()
+    max_employee_id = result['MAX(employee_id)'] if result['MAX(employee_id)'] else 0
+    return max_employee_id
+@app.route('/login/register', methods=["GET", "POST"])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password", "confirm-password" POST requests exist (user submitted form)
+    if request.method == 'POST':
+        # Create variables for easy access
+        username = request.form['username']
+        # Hash password using SHA256
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
+        is_active = 1
+        # Get the maximum employee_id in the database
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        max_employee_id = get_max_employee_id(cursor)
+        # Generate a new employee_id
+        employee_id = max_employee_id + 1
+        failed_login_attempts = 0
+        last_login_time = datetime.now()
+        password_reset_token = hashlib.sha256(request.form['password'].encode()).hexdigest()
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Auth_user WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password:
+            msg = 'Please fill out the form!'
+        elif password != confirm_password:
+            msg = 'Passwords do not match!'
+        elif len(password) < 8:
+            msg = 'Password must be at least 8 characters long!'
+        elif not any(char.isdigit() for char in password):
+            msg = 'Password must contain at least one number!'
+        elif not any(char.isupper() for char in password):
+            msg = 'Password must contain at least one uppercase letter!'
+        elif not any(char.islower() for char in password):
+            msg = 'Password must contain at least one lowercase letter!'
+        else:
+            # Account doesn't exist and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO Auth_user VALUES (NULL, %s, %s, %s, %s, %s, %s, %s)',
+                (username, password, employee_id, is_active, failed_login_attempts, last_login_time, password_reset_token,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == "POST":
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+@app.route('/login/users')
+def load_users():
+    # Check if user is logged-in
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select Employee.id as id, Employee.FirstName as firstname, Employee.LastName as lastname, info.jobname as job, info.departmentname as department, Employee.Age as age, Employee.Phone_no as phone_no, Employee.Email_Address as email, Employee.Address as address from Employee left join ( select Job.id as id, Job.Name as jobname, Department.Name as departmentname from Job left join Department on Job.department_id=Department.id ) as info on Employee.Job_ID=info.id')
+        employee = cursor.fetchall()
+        return render_template('user.html', employee=employee)
+    return redirect(url_for('login'))
+
+@app.route('/login/calendar')
+def calendar():
+    # Check if user is logged-in
+    if 'loggedin' in session:
+        return render_template('calendar.html')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
