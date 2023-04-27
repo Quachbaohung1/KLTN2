7946536d1c48f797b6c307992265f8acc9b23294
Quachbaohung1 <baohung9715@gmail.com>
2023-04-28 01:21:16 +0700

    1st commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,75 +1,75 @@
 (function() {
-  "use strict";
+    "use strict";
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
+    /*
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+          return [...document.querySelectorAll(el)]
+        } else {
+          return document.querySelector(el)
+        }
     }
-  }
 
-  /**
+  /*
    * Easy event listener function
    */
-  const on = (type, el, listener, all = false) => {
-    if (all) {
-      select(el, all).forEach(e => e.addEventListener(type, listener))
-    } else {
-      select(el, all).addEventListener(type, listener)
+    const on = (type, el, listener, all = false) => {
+        if (all) {
+          select(el, all).forEach(e => e.addEventListener(type, listener))
+        } else {
+          select(el, all).addEventListener(type, listener)
+        }
     }
-  }
 
-  /**
+  /*
    * Easy on scroll event listener 
    */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
 
-  /**
+  /*
    * Sidebar toggle
    */
-  if (select('.toggle-sidebar-btn')) {
-    on('click', '.toggle-sidebar-btn', function(e) {
-      select('body').classList.toggle('toggle-sidebar')
-    })
-  }
+   if (select('.toggle-sidebar-btn')) {
+        on('click', '.toggle-sidebar-btn', function(e) {
+            select('body').classList.toggle('toggle-sidebar')
+        })
+   }
 
-  /**
+  /*
    * Search bar toggle
    */
-  if (select('.search-bar-toggle')) {
-    on('click', '.search-bar-toggle', function(e) {
-      select('.search-bar').classList.toggle('search-bar-show')
-    })
-  }
+    if (select('.search-bar-toggle')) {
+        on('click', '.search-bar-toggle', function(e) {
+            select('.search-bar').classList.toggle('search-bar-show')
+        })
+    }
 
-  /**
+  /*
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
 
-  /**
+  /*
    * Toggle .header-scrolled class to #header when page is scrolled
    */
   let selectHeader = select('#header')
@@ -292,23 +292,22 @@
   /**
    * Initiate Datatables
    */
-  const datatables = select('.datatable', true)
-  datatables.forEach(datatable => {
-    new simpleDatatables.DataTable(datatable);
-  })
+    const datatables = select('.datatable', true)
+        datatables.forEach(datatable => {
+            new simpleDatatables.DataTable(datatable);
+        })
 
   /**
    * Autoresize echart charts
    */
-  const mainContainer = select('#main');
-  if (mainContainer) {
-    setTimeout(() => {
-      new ResizeObserver(function() {
-        select('.echart', true).forEach(getEchart => {
-          echarts.getInstanceByDom(getEchart).resize();
-        })
-      }).observe(mainContainer);
-    }, 200);
-  }
-
+    const mainContainer = select('#main');
+        if (mainContainer) {
+            setTimeout(() => {
+                new ResizeObserver(function() {
+                    select('.echart', true).forEach(getEchart => {
+                        echarts.getInstanceByDom(getEchart).resize();
+                    })
+                }).observe(mainContainer);
+                }, 200);
+        }
 })();
